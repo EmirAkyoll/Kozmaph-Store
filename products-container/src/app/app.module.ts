05a781254f638fe26ded023715ffa9e9fe05a253
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-11-26 18:07:37 +0300

    add footer

--- products-container/src/app/app.module.ts
+++ products-container/src/app/app.module.ts
@@ -6,36 +6,36 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { FavoritesComponent } from './components/favorites/favorites.component';
 import { ProductsComponent } from './components/products/products.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductOverviewComponent } from './components/product-detail/product-overview/product-overview.component';
 import { ProductInDepthComponent } from './components/product-detail/product-in-depth/product-in-depth.component';
 import { ProductCommentsComponent } from './components/product-detail/product-comments/product-comments.component';
 import { ProductQuestionsComponent } from './components/product-detail/product-questions/product-questions.component';
 
+import { TagModule } from 'primeng/tag';
 import { BadgeModule } from 'primeng/badge';
-import { GalleriaModule } from 'primeng/galleria';
+import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
-import { TagModule } from 'primeng/tag';
-import { AccordionModule } from 'primeng/accordion';
-import { RatingModule } from 'primeng/rating';
+import { GalleriaModule } from 'primeng/galleria';
 import { DataViewModule } from 'primeng/dataview';
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductCardComponent,
-    FavoritesComponent,
     ProductsComponent,
+    FavoritesComponent,
     CategoriesComponent,
+    ProductCardComponent,
     ProductDetailComponent,
-    ProductOverviewComponent,
     ProductInDepthComponent,
+    ProductOverviewComponent,
     ProductCommentsComponent,
     ProductQuestionsComponent,
   ],
