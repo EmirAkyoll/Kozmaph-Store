13b7eed78e43fc8bb5a61f0f0838194598e93236
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-11-30 20:49:47 +0300

    .

--- products-container/frontend/src/interfaces/product.interface.ts
+++ products-container/frontend/src/interfaces/product.interface.ts
@@ -19,3 +19,297 @@ export interface Product {
     rating?: number;
   }[];
 }
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
+export interface Product {
+  name: string;
+  image_urls: string[];
+  price: number;
+  seller: string;
+  advantages?: string[];
+  summary: string[];
+  description: string[];
+  features: {
+    feature_name: string;
+    feature_value: string;
+  }[];
+  questions?: {
+    question_text: string;
+    answers: string;
+  }[];
+  comments?: {
+    comment_text: string;
+    rating?: number;
+  }[];
+}
