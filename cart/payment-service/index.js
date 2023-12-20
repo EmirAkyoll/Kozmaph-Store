353a5210bb11615eae68d1db61c4d32320c1b431
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-12-20 19:24:56 +0300

    add payment service

---
+++ cart/payment-service/index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")("YOUR_SECRET_KEY");
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/create-checkout-session",async(req,res)=>{
+    const {products} = req.body;
+
+    const lineItems = products.map((product)=>({
+        price_data:{
+            currency:"usd",
+            product_data:{
+                name:product.name,
+                images:[product.image]
+            },
+            unit_amount:product.price * 100,
+        },
+        quantity:product.quantity
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:3000/sucess",
+        cancel_url:"http://localhost:3000/cancel",
+    });
+
+    res.json({id:session.id})
+ 
+})
+
+
+app.listen(7000,()=>{
+    console.log("payment server start on 7000")
+})
